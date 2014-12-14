4d23d6b2f88232e4d26595cde10b02dafab2af63
Joe Bellus <joe@5sigma.io>
2014-12-14 16:23:10 -0600

    Removed checkboxes from tables for batch actions

--- spec/dummy/db/seeds.rb
+++ spec/dummy/db/seeds.rb
@@ -1,5 +1,12 @@
 require "faker"
 
-10.times do 
-    Customer.create(first_name:Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, phone_number: rand(9).to_s*10)
+
+AppKit::User.create(email: 'admin@example.com', password: 'test123', password_confirmation: 'test123')
+10.times do
+  customer  = Customer.create(first_name:Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, phone_number: rand(9).to_s*10)
+    rand(5).times do
+      invoice = Invoice.create(customer: customer, invoice_date: Time.current, published: true, paid: true)
+      InvoiceItem.create(invoice: invoice, description: Faker::Commerce.product_name,
+                         unit_price: rand(100), quantity: rand(10))
+    end
 end
