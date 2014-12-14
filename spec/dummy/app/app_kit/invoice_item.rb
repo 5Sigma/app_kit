078e13f790387d99abca9e5d8294b9580c3a0575
Joe Bellus <joe@5sigma.io>
2014-12-13 20:23:40 -0600

    Navigation restyle
    Restyled the top navigation and added a dashboard item. Also changed the
    "home" icon in the breadcrumb to a "dashboard" icon.
    Restyled top navigation and header links

--- spec/dummy/app/app_kit/invoice_item.rb
+++ spec/dummy/app/app_kit/invoice_item.rb
@@ -1,16 +1,6 @@
 AppKit.register InvoiceItem do
-  # Create a menu item for this resource in the main navigation bar.
   show_in_navigation true
-
-  # Associate an icon with this item. FontAwesome is used to produce icons
-  # This can be the name of any FontAwesome icon
-  # icon 'user'
-  # Field configuration
-  # Possible field options can be
-  #   show_in_table (true|false) to show in the field in tables
-  #   show_in_details (true|false) to show the field when the record is viewed
-  #   formatter (string) - a formatter that should be used to display the field
-  #   hide - hides the field from both tables and detail views
+  icon 'list-alt'
 
   field :id
   field :invoice_id
@@ -21,13 +11,6 @@ AppKit.register InvoiceItem do
   field :created_at
   field :updated_at
 
-  # Action definitions - Custom actions can be defined for the resource. This
-  # will generate a link on the details page for the record to perform the 
-  # given action
-  #
-  # action :some_action do |invoice_item|
-  #   invoice_item.my_action(param: true)
-  # end
 end
 
 
