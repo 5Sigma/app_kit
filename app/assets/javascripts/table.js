1ea13ed1b2b56248b69faab36c6d4f054b97488a
Joe Bellus <joe@5sigma.io>
2014-12-15 02:03:58 -0600

    Fixed bug in association tables in resource show
    Fixed the indentation in the resource show view so that the table cells
    where inside the table rows in the association tables

--- app/assets/javascripts/table.js
+++ app/assets/javascripts/table.js
@@ -1,9 +1,6 @@
 $(function() {
     $('table tr.data-row td').click(function(e) {
         row_url = $(this).parent().data('url')
-        if ($(this).hasClass('checkbox-cell')) { 
-            return; 
-        }
         window.location.href =  row_url
     });
 })
